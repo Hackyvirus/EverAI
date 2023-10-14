05d11fad6f8c5324e9d49d48ad8d238dba8dd848
sushant <sushantatram591081@gmail.com>
2023-10-14 22:49:40 +0530

    Final Deployment

--- views/script.js
+++ views/script.js
@@ -1,10 +1,13 @@
+
 let login = document.getElementById("login")
-document.getElementById("login").addEventListener("click",()=>{
-        document.querySelector(".introtoAI").style.display="none";
-        document.querySelector(".login-box").style.display="block"
+document.getElementById("login").addEventListener("click", () => {
+        document.querySelector(".introtoAI").style.display = "none";
+        document.querySelector(".login-box").style.display = "block"
 })
-document.getElementById("signup").addEventListener("click",()=>{
-        document.querySelector(".introtoAI").style.display="none";
-        document.querySelector(".login-box").style.display="block"
+document.getElementById("signup").addEventListener("click", () => {
+        document.querySelector(".introtoAI").style.display = "none";
+        document.querySelector(".login-box").style.display = "block"
 })
+
+alert("This is Beta Version, So we have desabled some of features like 'Roles' and 'Languages' we are going to enable them when we are lauching it for Tablet and Mobiles.")
 
