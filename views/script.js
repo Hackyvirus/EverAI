a1d11ce4d7968520b8c5d0a4ddfca3d4759fcafa
sushant <sushantatram591081@gmail.com>
2023-10-15 00:54:24 +0530

    Final Deployment

--- views/script.js
+++ views/script.js
@@ -9,5 +9,5 @@ document.getElementById("signup").addEventListener("click", () => {
         document.querySelector(".login-box").style.display = "block"
 })
 
-alert("This is Beta Version, So we have disabled some of features like 'Roles' and 'Languages' we are going to enable them when we are launching it for Tablet and Mobiles.")
+
 
